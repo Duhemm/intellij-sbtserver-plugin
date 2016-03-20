5c922a09a5fb8ceefe097e14975ae43b2f1eb4c6
Eugene Yokota <eed3si9n@gmail.com>
2016-03-20 00:09:29 -0400

    Fix some of the missing parts

--- src/main/scala/sbt/intellij/sbtsupport/BuildOnSbtServerAction.scala
+++ src/main/scala/sbt/intellij/sbtsupport/BuildOnSbtServerAction.scala
@@ -1,16 +1,13 @@
-package com
-package lightbend
-package sbtserversupport
+package sbt.intellij.sbtsupport
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.lightbend.intellijson.Command
 
 class BuildOnSbtServerAction extends AnAction {
   @Override
   def actionPerformed(e: AnActionEvent): Unit = {
     val consoleComponent = e.getProject.getComponent(classOf[SbtServerConsole])
-    val command = Command("compile")
+    val command = CommandExec("compile")
     consoleComponent.tellSbtTo(command)
   }
 }
